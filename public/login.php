a9184242451fa7beae8d34249eddb5fead9b27b6
Fmora17 <fmoragarita@gmail.com>
2025-10-20 10:12:28 -0600

    Se agrego código de estilo

--- public/login.php
+++ public/login.php
@@ -26,9 +26,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   <meta name="viewport" content="width=device-width,initial-scale=1">
   <title>Iniciar Sesión - Inventario</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="css/styles.css">
+  <link rel="stylesheet" href="css/style.css">
 </head>
-<body class="bg-body">
+<body class="login-body">
           <div class="login-wrapper">
            <div class="card">
             <div class="card-body">
