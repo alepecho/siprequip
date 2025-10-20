694238df1b101756029891b18ef0f55ab248f3df
Fmora17 <fmoragarita@gmail.com>
2025-10-20 10:01:53 -0600

    Se agrego código de estilo

--- public/login.php
+++ public/login.php
@@ -28,13 +28,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="css/styles.css">
 </head>
-<body class="bg-light">
-  <div class="container py-5">
-    <div class="row justify-content-center">
-      <div class="col-md-5">
-        <div class="card shadow-lg border-0 rounded-4">
-          <div class="card-body p-4">
-            <h3 class="card-title text-center text-primary mb-4 fw-bold">Inicio de Sesión</h3>
+<body class="bg-body">
+          <div class="login-wrapper">
+           <div class="card">
+            <div class="card-body">
+              <h3 class="card-title text-center text-primary mb-4 fw-bold">Inicio de Sesión</h3>
 
             <?php if ($errors): ?>
               <div class="alert alert-danger text-center">
