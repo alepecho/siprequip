3a1df984ae28680aff56fbfe2df66825b80ee572
Fmora17 <fmoragarita@gmail.com>
2025-10-23 09:15:19 -0600

    db

--- includes/db.php
+++ includes/db.php
@@ -4,20 +4,8 @@ $user = "root";           // Usuario por defecto en XAMPP
 $password = "";           // Contraseña (vacía por defecto en XAMPP)
 $dbname = "empleados";    // Nombre de tu base de datos
 
-<<<<<<< HEAD
-//conexion 
-$server='localhost';
-$username='root';
-$password='';
-$database='';
-$db=mysqli_connect($server, $username, $password,$database);
 
-mysqli_query($db, "SET NAMES 'utf8'");
 
-//Iniciar la sesion
-
-session_start();
-=======
 // Crear conexión
 $conn = new mysqli($host, $user, $password, $dbname);
 
@@ -27,4 +15,4 @@ if ($conn->connect_error) {
 }
 echo "Conexión exitosa";
 ?>
->>>>>>> BackEnd
+
