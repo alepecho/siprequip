e6903d0be62220a69bfa7fad7c03aae04639b09c
Brandon-bit320 <bsanchez25031@gmail.com>
2025-10-23 09:50:04 -0600

    Resolución de conflicto

--- includes/db.php
+++ includes/db.php
@@ -5,10 +5,6 @@ $password = "";           // Contraseña (vacía por defecto en XAMPP)
 $dbname = "empleados";    // Nombre de tu base de datos
 
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 9c326f7858dbe2d3b4e417b824449b9e31bccea6
 // Crear conexión
 $conn = new mysqli($host, $user, $password, $dbname);
 
@@ -18,7 +14,3 @@ if ($conn->connect_error) {
 }
 echo "Conexión exitosa";
 ?>
-<<<<<<< HEAD
-
-=======
->>>>>>> 9c326f7858dbe2d3b4e417b824449b9e31bccea6
